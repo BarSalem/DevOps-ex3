9859390e9363be3ae0f44ebb4d114fd263ff9f8a
BarSalem <barsalem555@gmail.com>
2022-11-17 20:58:44 +0200

    Changes to BarSalem.java File

--- BarSalem.java
+++ BarSalem.java
@@ -1,7 +1,7 @@
 public class BarSalem{
 	public static void main(String[] args){
 		for (int i=1;i<=10;i++){
-			System.out.println("My name is Bar Salem");
+			System.out.println("My name is Bar Salem and this is the 3rd commit which includes changes in BarSalem.java File");
 		}
 	}
 }
